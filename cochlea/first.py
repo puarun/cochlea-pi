b71b4aa51fd2e2ec19389056928a5c7bdb2bbaa9
puarun <puarun@hotmail.com>
2020-08-16 03:23:35 +0100

    Adding separate folder for cochlea

---
+++ cochlea/first.py
@@ -0,0 +1,13 @@
+"""
+Hello we are attempting to simulate the cochlea.
+The cochlea uses a concept called tonotopy i.e. low frequencies are processed at the apex 
+and high frequencies are processed at the base. Furthermore sound travels to its point 
+of transduction as a traveling wave
+"""
+def __self_description():
+    print("Simulation of a traveling wave...enjoy")
+
+if __name__ == "__main__":
+    __self_description()
+else:
+    "Using first.py as an import"
