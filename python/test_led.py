d9eada21b07e60d63b0f1219ec04ea51e532edfd
puarun <puarun@hotmail.com>
2020-08-16 02:03:14 +0100

    first commit

---
+++ python/test_led.py
@@ -0,0 +1,14 @@
+import time
+import board
+import neopixel
+
+pixel_pin = board.D18
+
+# The number of NeoPixels
+num_pixels = 144
+ORDER = neopixel.GRB
+pixels = neopixel.NeoPixel(
+    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+)
+pixels.fill((0, 0, 0))
+pixels.show()
